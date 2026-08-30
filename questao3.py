f239d44bf4f100ea196283e799cd39b4cecd8d5c
Vanderleia <mariavanderleiamoliveira@gmail.com>
2026-08-30 11:54:50 -0300

    27-08-26, quetao 3

---
+++ questao3.py
@@ -0,0 +1,11 @@
+n1_texto = input('digite o primeiro nomero ')
+n2_texto = input('digite o segundo numero ')
+
+n1 = int(n1_texto)
+n2 = int(n2_texto)
+
+quociente = n1 // n2
+resto = n1 % n2
+
+print(quociente)
+print(resto)
