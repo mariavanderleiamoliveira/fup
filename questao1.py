7a3af578d9c96d675a50df2520973d128154193b
Vanderleia <mariavanderleiamoliveira@gmail.com>
2026-08-30 11:18:12 -0300

    27-08-26, questao 1

---
+++ questao1.py
@@ -0,0 +1,9 @@
+n1_texto = input('digite o primeiro nomero ')
+n2_texto = input('digite o segundo numero ')
+
+n1 = int(n1_texto)
+n2 = int(n2_texto)
+
+multiplicacao = n1 * n2
+
+print(multiplicacao)
