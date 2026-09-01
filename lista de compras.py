b32025a75a0909db2a5b2bc366f4eff84e67da0e
Vanderleia <mariavanderleiamoliveira@gmail.com>
2026-09-01 18:18:20 -0300

    lista de compras

---
+++ lista de compras.py
@@ -0,0 +1,17 @@
+n1_feijao = input ('digite o numero de itens feijao ')
+n2_acucar = input ('digite o numero de itens acucar ')
+n3_cafes = input ('digite o numero de itens cafes ')
+n4_arroz = input ('digite o numeros de itens arroz ')
+
+
+n1 = int(n1_feijao)
+n2 = int(n2_acucar)
+n3 = int(n3_cafes)
+n4 = int(n4_arroz)
+
+
+soma = n1 + n2 + n3 + n4
+
+
+print(soma)
+
