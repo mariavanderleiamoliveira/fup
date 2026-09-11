1bbbed8c7fb802a9c79d20d59118d13097e663af
Vanderleia <mariavanderleiamoliveira@gmail.com>
2026-09-11 19:30:18 -0300

    10-09-26, questão14

---
+++ questao14.py
@@ -0,0 +1,12 @@
+n1_texto = input('digite um numero do teclado ')
+
+n = int(n1_texto)
+
+if n > 0:
+    print('numero positivo')
+
+elif n == 0:
+    print('numero igual a zero')
+     
+else:
+    print('numero negativo')
