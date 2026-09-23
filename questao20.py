34d44b7057cb5974ef8bb5393881f5ad1dec0a4d
Vanderleia <mariavanderleiamoliveira@gmail.com>
2026-09-23 15:18:16 -0300

    questao 20,  23-09-26

--- questao20.py
+++ questao20.py
@@ -6,8 +6,21 @@ n1 = int(n1_texto)
 n2 = int(n2_texto)
 n3= int(n3_texto)
 
-if n1>n2 and n1>n3:
+if n1<n2 and n1<n3:
     if n2>n3:
-        print(f'a ordem dos numeros sao {n1}, {n2}, {n3})
+        print(f'a ordem dos numeros sao {n1}, {n2}, {n3}')
 
-elif n1>n3 and n2>1
+    elif n1<n3 and n3<n2:
+        print(f'a ordem dos numeros sao {n1}, {n3}, {n2}')
+
+elif n2<n1 and n1<n3:
+    print(f' a ordem dos numeros sao {n2}, {n1},{n3}')
+
+elif n2<n3 and n3<n1:
+    print(f'a ordem dos numeros sao {n2}, {n3}, {n1}')
+
+elif n3<n1 and n1<n2:
+    print(f'a ordem dos numeros sao {n3}, {n1}, {n2}')
+
+else:
+    print(n3, n2, n1)
